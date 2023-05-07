13ab2c2d13add6640de4c1d23f3727b63789b099
venky <venky94.vk@gmail.com>
2023-05-07 12:39:40 +0530

    [Venkatesh]PhonePattern file added

---
+++ PhonePattern/Mobile.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PhonePattern
+{
+    public class Mobile
+    {
+        static string PhonePattern = @"^[0-9]{2,3}\s[0-9]{10}$";
+
+        public void Phone()
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.Write("Enter Phone Number:");
+                string number = Convert.ToString(Console.ReadLine());
+                var flags = Regex.IsMatch(number, PhonePattern);
+
+                if (flags)
+                {
+                    Console.WriteLine("Phone number is Valid");
+                    flag = false;
+                }
+                else
+                {
+                    Console.WriteLine("Phone number is Invalid");
+
+                }
+            }
+        }
+    }
+}
---
+++ PhonePattern/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PhonePattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Mobile mobile = new Mobile();
+            mobile.Phone();
+        }
+    }
+}
