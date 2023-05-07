97c5f38cfbb1db842a4866b2dcb7790a7bff1ce2
venky <venky94.vk@gmail.com>
2023-05-07 12:47:29 +0530

    [Venkatesh]EmailSample file Added

---
+++ RegexEmailSamples/EmailSamples.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexEmailSamples
+{
+    internal class EmailSamples
+    {
+        static string EmailPattern = @"^[a-z0-9.]{3,20}@[a-z]{3,10}.(com|co|in)$";
+
+        public void Email()
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.Write("Enter Email:");
+                string email = Convert.ToString(Console.ReadLine());
+                var flags = Regex.IsMatch(email, EmailPattern);
+
+                if (flags)
+                {
+                    Console.WriteLine("Email is Valid");
+                    flag = false;
+                }
+                else
+                {
+                    Console.WriteLine("Email is Invalid");
+
+                }
+            }
+        }
+    }
+}
---
+++ RegexEmailSamples/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace RegexEmailSamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EmailSamples emailSamples = new EmailSamples();
+            emailSamples.Email();
+        }
+    }
+}
