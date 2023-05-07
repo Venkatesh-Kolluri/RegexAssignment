3802ccb4f79192f170ccbaf496cb32eba120226e
venky <venky94.vk@gmail.com>
2023-05-07 12:37:06 +0530

    [Venkatesh]EmailPattern file added

---
+++ RegexEmailPattern/Email.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexEmailPattern
+{
+    public class Email
+    {
+        static string EmailPattern = @"^[a-z.]{3,20}@[a-z]{2}.co.(com|in)$";
+        public void Mail()
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter Email: ");
+                string email = Convert.ToString(Console.ReadLine());
+                flag = Regex.IsMatch(email, EmailPattern);
+                if (flag)
+                {
+                    Console.WriteLine("Valid Email ");
+                }
+                else
+                {
+                    Console.WriteLine("InValid Email");
+                }
+
+
+            }
+
+        }
+    }
+}
---
+++ RegexEmailPattern/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace RegexEmailPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Email email = new Email();
+            email.Mail();
+        }
+    }
+}
