7ecb9c90e9c1df00cea88d2930d1e4054271c238
venky <venky94.vk@gmail.com>
2023-05-07 12:30:37 +0530

    [Venkatesh]FirstName File Added

---
+++ RegexAssignment/FirstName.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexAssignment
+{
+    public class FirstName
+    {
+        static string NamePattern = @"^[A-Z]{1}[a-z]{3,}$";
+        public void Name()
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter Name: ");
+                string name =Convert.ToString(Console.ReadLine());
+                flag=Regex.IsMatch(name, NamePattern);
+                if (flag)
+                {
+                    Console.WriteLine("Valid Name Pattern");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Name Pattern");
+                }
+
+            }
+            
+        }
+    }
+}
---
+++ RegexAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace RegexAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            FirstName firstName = new FirstName();
+            firstName.Name();
+
+        }
+    }
+}
