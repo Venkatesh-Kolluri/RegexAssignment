2cb4fe56cc69d6cfe583096793014b0a4822970c
venky <venky94.vk@gmail.com>
2023-05-07 12:41:55 +0530

    [Venkatesh]PasswordPattern1 Added

---
+++ RegexPassword1/PasswordCharacters.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexPassword1
+{
+    public class PasswordCharacters
+    {
+        static string PasswordPattern = @"^[A-Za-z0-9!@#$%^&*()<>~?]{8}$";
+
+        public void Password()
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.Write("Enter Password:");
+                string pass = Convert.ToString(Console.ReadLine());
+                var flags = Regex.IsMatch(pass, PasswordPattern);
+
+                if (flags)
+                {
+                    Console.WriteLine("Valid Password");
+                    flag = false;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Password");
+                }
+            }
+
+        }
+    }
+}
---
+++ RegexPassword1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegexPassword1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           PasswordCharacters passwordCharacters = new PasswordCharacters();
+            passwordCharacters.Password();
+        }
+    }
+}
