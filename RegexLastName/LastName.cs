d59a5294a07d0cf3c21178222a1fab8358bc0fa1
venky <venky94.vk@gmail.com>
2023-05-07 12:33:10 +0530

    [Venkatesh]LastName File Added

---
+++ RegexLastName/LastName.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexLastName
+{
+    internal class LastName
+    {
+        static string NamePattern = @"^[A-Z]{1}[a-z]{3,}(\s|_)[A-Z]{1}[a-z]{3,}$";
+        public void Name()
+        {
+            bool flag = true;
+            while (flag)
+            {
+                Console.Write("Enter FullName: ");
+                string fullName=Convert.ToString(Console.ReadLine());
+                flag = Regex.IsMatch(fullName,NamePattern);
+                if (flag)
+                {
+                    Console.WriteLine("Valid FullName");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid FullName");
+                }
+            }
+        }        
+    }
+}
---
+++ RegexLastName/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace RegexLastName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LastName lastName = new LastName();
+            lastName.Name();
+        }
+    }
+}
